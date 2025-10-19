2aae20e69617b5e756e7728bc632016c84e7c8b7
WadoodWasay <wadoodwasay2008@gmail.com>
2025-10-19 12:59:38 -0400

    forgot to change to 512 word in netlists_FPGA.v

--- Caravel/src/netlists_FPGA.v
+++ Caravel/src/netlists_FPGA.v
@@ -33,5 +33,5 @@
 //`include "../../mgmt_soc/src/VexRiscv_MinDebugCache.v"
 `include "../../arty/uart_to_spi.v"
 //new cores being tested (in order of size) 
-//`include "../../mgmt_soc/src/cache_changes_testing/VexRiscv_MinDebug_512Word.v"
-`include "../../mgmt_soc/src/cache_changes_testing/VexRiscv_MinDebug_1024Word.v"
+`include "../../mgmt_soc/src/cache_changes_testing/VexRiscv_MinDebug_512Word.v"
+//`include "../../mgmt_soc/src/cache_changes_testing/VexRiscv_MinDebug_1024Word.v"
